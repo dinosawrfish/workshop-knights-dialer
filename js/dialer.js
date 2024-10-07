9c3960e4cf93b175f793134ed79430b2f792e06a
dinosawrfish <dinosawrfish@gmail.com>
2024-10-07 14:15:38 -0700

    pre compute reachable keys

--- js/dialer.js
+++ js/dialer.js
@@ -8,39 +8,20 @@ export default {
 // ****************************
 
 var dialpad = [
-	[1, 2, 3],
-	[4, 5, 6],
-	[7, 8, 9],
-	[ , 0,  ]
+	[4, 6],
+	[6, 8],
+	[7, 9],
+	[4, 8],
+	[3, 9, 0],
+	[],
+	[1, 7, 0],
+	[2, 6],
+	[1, 3],
+	[2, 4]
 ];
 
 function reachableKeys(startingDigit) {
-	var nearbyKeys = [];
-
-	for (let [rowIdx, row] of dialpad.entries()) {
-		let colIdx = row.indexOf(startingDigit);
-		if (colIdx != -1) {
-			for (let rowMove of [-2, -1, 1, 2]) {
-				for (let colMove of [-2, -1, 1, 2]) {
-					if (Math.abs(rowMove) != Math.abs(colMove)) {
-						if (
-							rowIdx + rowMove >= 0 &&
-							rowIdx + rowMove <=3 &&
-							colIdx + colMove >=0 &&
-							colIdx + colMove <= 2 &&
-							dialpad
-								[rowIdx + rowMove]
-								[colIdx + colMove] != undefined
-						) {
-							nearbyKeys.push(dialpad[rowIdx + rowMove][colIdx + colMove])
-						}
-					}
-				}
-			}
-		}
-	}
-
-	return nearbyKeys;
+	return nearbyKeys[startingDigit];
 }
 
 function countPaths(startingDigit,hopCount) {
